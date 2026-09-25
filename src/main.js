626931c87c082736a012df27e0ec099bc9c60dfa
stone <xijieyin@gmail.com>
2026-09-25 09:13:04 +0800

    Release v0.1.8

--- src/main.js
+++ src/main.js
@@ -49,7 +49,7 @@ app.connect('startup', () => {
     const about = new Adw.AboutDialog({
       application_name: 'Wallshader',
       application_icon: APP_ID,
-      version: '0.1.7',
+      version: '0.1.8',
       developer_name: 'Wallshader contributors',
       comments:
         'A little color for your desktop. Native GNOME wallpapers made with Paper Shaders.',
